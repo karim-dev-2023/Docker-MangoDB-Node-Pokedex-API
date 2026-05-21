86b146ac5992e03972ffb478f27fb4d6ecc5fc3e
Karim <karim.ayoub.job@gmail.com>
2026-05-22 00:57:10 +0200

    refacot: mangodb config for docker

---
+++ api/src/config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const seedPokemon = require("../seeders/pokemonSeeder.js"); 
+
+mongoose.connect('mongodb+srv://mn_db_user:1844@cluster0.bhix0hm.mongodb.net/pokemons')
+.then(async () => {
+  console.log("✅ Connecté à MongoDB");
+
+  await seedPokemon();
+
+})
+.catch(err => console.error("❌ Erreur MongoDB :", err));
+
+module.exports = mongoose;
